c971f8f766d98c33ba4b40fb42c24006c802282b
mzr777 <405860242@qq.com>
2017-10-30 18:35:37 +0800

    Initial commit

---
+++ app/src/main/java/com/bwie/a01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bwie.a01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.print("甜蜜暴击甜蜜暴击");
+    }
+}
